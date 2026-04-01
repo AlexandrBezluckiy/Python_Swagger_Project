1e3f51329dbb974ca21861e2310130765f3062ae
AlexandrBezluckiy <a.bezluckiy@gmail.com>
2026-04-01 10:24:23 +0700

    add test_delete_pet_from_store

--- utils/client.py
+++ utils/client.py
@@ -2,6 +2,7 @@ import requests
 
 
 base_url = 'https://petstore.swagger.io/v2'
+api_key = 'special-key'
 
 def send_request(method, url, **kwargs):
     return requests.request(method, url, **kwargs)
